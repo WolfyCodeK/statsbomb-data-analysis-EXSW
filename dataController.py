20e81ca70345e9a6d8d298c0048164ad512b5838
WolfyCodek <45407550+WolfyCodeK@users.noreply.github.com>
2023-03-16 16:39:21 +0000

    Started using docstring convention commenting.
    See -> jsonToString function

--- dataController.py
+++ dataController.py
@@ -1,6 +1,14 @@
 import json
 
 def jsonToString(path):
+    """ 
+    jsonToString converts a json file to a raw String
+    
+    Keyword Arguments:
+    path: Relative path to json file
+    return: Json file in a raw String format   
+    """
     with open(path, "r") as read_file:
         strJson = json.load(read_file)
+        
     return strJson
