9af0c5d8d6af60870c1db176e4c49fe359812576
harvey <harveybellini@gmail.com>
2023-03-18 17:31:28 +0000

    rons flying, removed the unncessary arena space and his podium

--- rendertest/main.py
+++ rendertest/main.py
@@ -2,17 +2,17 @@ import vtkplotlib as vpl
 from stl.mesh import Mesh
 
 halfron = "rendertest\halfron.stl"
-stadium = "rendertest\EuroArena.stl"
+stadium_stl = "rendertest\EuroArena.stl"
+textures_dir = "rendertest\Textures"
 
 # Read the STL using numpy-stl
 mesh = Mesh.from_file(halfron)
-stadium = Mesh.from_file(stadium)
-
+stadium = Mesh.from_file(stadium_stl)
 
 # Plot the mesh
 vpl.mesh_plot(mesh)
 vpl.mesh_plot(stadium)
 
 # Show the figure
-vpl.view(camera_position=[0,0,10])
+vpl.view(camera_position=[0,1,0])
 vpl.show()
