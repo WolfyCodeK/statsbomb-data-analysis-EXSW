27257c68bce3cc26c27313dfc35858dfb4d20955
harvey <harveybellini@gmail.com>
2023-03-25 11:51:55 +0000

    db

--- MCI/mancity/pitch/views.py
+++ MCI/mancity/pitch/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+import sqlite3 as sl
+
+'''
+
+Select match you want
+Select the player you pass from and to
+List all the times that event happened
+
+
+
+'''
 
 def main(request):
+    statsbombDB = sl.connect("C:/Users/harve/OneDrive/Desktop/EXSW/statsbomb-data-analysis-EXSW/MCI/mancity/pitch/static/pitch/databases/statsbombDatabase_ManCity_Arsenal_events.db")
+    dbCursor = statsbombDB.cursor()
+
     players = get_unique_players()
     pass_count = 0
     sender_id = None
@@ -12,10 +26,22 @@ def main(request):
         sender_id = request.POST.get("sender")
         receiver_id = request.POST.get("receiver")
         pass_count = passtest(sender_id, receiver_id)
+        dbCursor.execute(getQueryNumOfPassesBetweenPlayers(player1=sender_id, player2=receiver_id))
+
+        rows = dbCursor.fetchall()
+        print(rows)
+        #len rows is passcount
+        if len(rows) != 0:
+            pass_count=len(rows)
+
+        '''
+        Xcoord, y coord, min, second
+        '''
 
     return render(request, 'pitch/pitch.html', {'players': players, 'pass_count': pass_count, 'sender_id': sender_id, 'receiver_id': receiver_id})
 
 def get_unique_players():
+
     output = 'pitch/static/pitch/ManCity_Arsenal_events.json'
 
     # Load the JSON data
@@ -60,3 +86,25 @@ def passtest(sender_id, receiver_id):
             pass
 
     return pass_count
+
+
+#input player data
+def getQueryNumOfPassesBetweenPlayers(player1, player2):
+    query = """
+        SELECT location_x, location_y, minute, second
+        FROM (
+            SELECT EVENT.id, EVENT.player_id, location_x, location_y, minute, second
+            FROM EVENT
+            JOIN PLAYER ON EVENT.player_id = PLAYER.id
+        ) AS T1 JOIN PASS ON PASS.event_id = T1.id
+        WHERE (T1.player_id = """ + str(player1) + """
+        AND recipient_id = """+ str(player2) + """)
+        OR (T1.player_id = """ + str(player2) + """
+        AND recipient_id = """ + str(player1) + """)
+        """
+
+    return query
+
+
+
+
