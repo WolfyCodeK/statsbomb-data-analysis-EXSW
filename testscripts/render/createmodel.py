22d15cc8ef41547b977809d4dc3c799581b53c68
harvey <harveybellini@gmail.com>
2023-03-27 17:30:16 +0100

    changes

--- testscripts/render/createmodel.py
+++ testscripts/render/createmodel.py
@@ -1,31 +1,26 @@
 import bpy
 import math
+
+def default_rotate(obj):
+    angle = math.radians(-90)
+    axis = 'X'
+    obj.rotation_euler.rotate_axis(axis, angle)
+    return obj
+
+outputloc="testscripts/render/output.obj"
+playermodelloc="models\halfron.obj"
+pitchloc="models\Soccer Field With Field Texture Big.fbx"
+
 # Load the halfron.obj file
-bpy.ops.import_scene.obj(filepath="models\halfron.obj")
+bpy.ops.import_scene.obj(filepath=playermodelloc)
 
 # Get a reference to the object
 obj = bpy.context.selected_objects[0]
 
 # Set the object's location to 0,0,0
 obj.location = (0, 0, 0)
-dup_obj = obj.copy()
-
-#rotate
-# Rotate the object by 45 degrees around the Z-axis
-angle = math.radians(-90)
-axis = 'X'
-obj.rotation_euler.rotate_axis(axis, angle)
-
-# Create a duplicate of the object
-bpy.context.scene.collection.objects.link(dup_obj)
-
-# Set the duplicate object's location to 1,1,1
-dup_obj.location = (10000, 10000, 1)
-
-# Select both objects
-bpy.context.view_layer.objects.active = obj
-obj.select_set(True)
-dup_obj.select_set(True)
+default_rotate(obj)
 
 # Export the scene as an OBJ file
-bpy.ops.export_scene.obj(filepath="testscripts/render/output.obj", check_existing=False, use_selection=True)
+bpy.ops.export_scene.obj(filepath=outputloc, check_existing=False, use_selection=True)
+
