ad31cf03bbb3e2124e40086ed235ab805568e588
harvey <harveybellini@gmail.com>
2023-03-29 00:31:28 +0100

    added ball movement

--- testscripts/render/createmodel.py
+++ testscripts/render/createmodel.py
@@ -53,6 +53,8 @@ def change_material_color_to_red(obj):
     if bsdf_node:
         bsdf_node.inputs['Base Color'].default_value = (1, 0, 0, 1)
 
+    return obj
+
 def makeobject(*obj_name):
     # Load the halfron.obj file
     bpy.ops.import_scene.obj(filepath=playermodelloc)
@@ -65,6 +67,22 @@ def makeobject(*obj_name):
     default_rotate(obj_name)
     return obj_name
 
+def makeball(*obj_name):
+    # Load the ball.obj file
+    bpy.ops.import_scene.obj(filepath=ballloc)
+
+    # Get a reference to the object
+    obj_name = bpy.context.selected_objects[0]
+
+    # Set the object's location to 0,0,0
+    
+    obj_name.scale = (15000, 15000, 15000)
+    obj_name.location = (33000 *ballpos[1][0],33000 *ballpos[1][1],(33000 *ballpos[1][2]))
+
+
+    default_rotate(obj_name)
+    return obj_name
+
 def setcoords(obj,coordinates):
     obj.location = (33000 * coordinates[0],33000 * coordinates[1],33000 * coordinates[2])
     return obj
@@ -73,11 +91,12 @@ def setcoords(obj,coordinates):
 outputloc = "testscripts/render/output.obj"
 mtlloc = "testscripts/render/output.mtl"
 playermodelloc = "models/halfron.obj"
+ballloc = "models/Ball/Ball.obj"
 pitchloc = "models/Soccer Field With Field Texture Big.fbx"
 matchdata="C:/Users/harve/Downloads/MCI Women's Files/g2312135_SecondSpectrum_tracking-produced.xml"
 teamlineuploc="C:/Users/harve/Downloads/MCI Women's Files/g2312135_SecondSpectrum_meta.json"
 
-def get_player_locations(matchdata=matchdata, input_time=0.04):
+def get_player_locations(matchdata=matchdata, input_time=0.0):
     root = ET.parse(matchdata).getroot()
     player_locations = []
 
@@ -90,6 +109,8 @@ def get_player_locations(matchdata=matchdata, input_time=0.04):
                 elif player.tag == 'ball':
                     id = 'ball'
                     loc = player.get('loc')
+                    global ballpos 
+                    ballpos = [id,ast.literal_eval(loc)]
                 player_locations.append([id, loc])
             break
 
@@ -98,9 +119,10 @@ def get_player_locations(matchdata=matchdata, input_time=0.04):
 objs_to_render=[]
 
 def createallplayers(teams):
-    player_locations = get_player_locations(matchdata,input_time=0.04)
+    player_locations = get_player_locations(matchdata,input_time=0.0)
     for element in player_locations:
         if element[0] != "ball":
+            print(element)
             #home team
             if any(element[0] in sublist for sublist in teams[0]):
                 print("home team")
@@ -116,10 +138,6 @@ def createallplayers(teams):
                 temp_obj = change_material_color_to_red(temp_obj)
                 temp_obj = setcoords(temp_obj,ast.literal_eval(element[1]))
                 objs_to_render.append(temp_obj)
-            
-        if element[0] == "ball":
-            print("add ball model to list")
-
 
 def export_objects_to_obj(objs_to_render, outputloc):
     # Deselect all objects
@@ -164,5 +182,7 @@ def getteamplayerlist():
 
 teams = getteamplayerlist()
 createallplayers(teams)
+ball = makeball()          
+objs_to_render.append(ball)
 print(objs_to_render)
 export_objects_to_obj(objs_to_render, outputloc)
