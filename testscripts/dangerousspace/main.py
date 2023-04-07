e7a0f9bc921e0267bc8069f3af9b240d8799cec8
harvey <harveybellini@gmail.com>
2023-04-07 16:58:38 +0100

    loading jsons

--- testscripts/dangerousspace/main.py
+++ testscripts/dangerousspace/main.py
@@ -1,45 +1,28 @@
 import json
+import os
 
-def load_json_data(json_data):
-    data = []
-    for game_data in json_data:
-        data.append(json.loads(game_data))
-    return data
+def process_jsonl_file(filename):
+    data_list = []
+    with open(filename, 'r') as file:
+        for line in file:
+            data = json.loads(line)
+            data_list.append(data)
+    return data_list
 
-def calculate_average_positions(data):
-    total_positions = {}
-    num_frames = len(data)
+directory = "largefiles"
+filenames = [
+    "g2312135_SecondSpectrum_tracking-produced.jsonl",
+    "g2312213_SecondSpectrum_tracking-produced.jsonl",
+    "g2312152_SecondSpectrum_tracking-produced.jsonl",
+    "g2312166_SecondSpectrum_tracking-produced.jsonl",
+    "g2312183_SecondSpectrum_tracking-produced.jsonl"
+]
 
-    for frame in data:
-        for player in frame["homePlayers"] + frame["awayPlayers"]:
-            player_id = player["playerId"]
-            if player_id not in total_positions:
-                total_positions[player_id] = {"xyz": [0, 0, 0], "count": 0}
-            total_positions[player_id]["xyz"][0] += player["xyz"][0]
-            total_positions[player_id]["xyz"][1] += player["xyz"][1]
-            total_positions[player_id]["xyz"][2] += player["xyz"][2]
-            total_positions[player_id]["count"] += 1
+data_variables = []
 
-    average_positions = {}
-    for player_id, player_data in total_positions.items():
-        average_positions[player_id] = [
-            coord / player_data["count"] for coord in player_data["xyz"]
-        ]
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+    data = process_jsonl_file(filepath)
+    data_variables.append(data)
 
-    return average_positions
-
-def main():
-    json_data = [
-        '{"period": 1, "frameIdx": 0, ...}',
-        '{"period": 1, "frameIdx": 1, ...}',
-        # ...
-    ]
-
-    data = load_json_data(json_data)
-    average_positions = calculate_average_positions(data)
-
-    for player_id, position in average_positions.items():
-        print(f"Player {player_id} average position: {position}")
-
-if __name__ == "__main__":
-    main()
+data1, data2, data3, data4, data5 = data_variables
