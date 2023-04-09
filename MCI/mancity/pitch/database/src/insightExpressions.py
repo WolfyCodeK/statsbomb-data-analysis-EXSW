1aa4baa51c8e337f71da004358a5729bb9d5dc8d
WolfyCodeK <45407550+WolfyCodeK@users.noreply.github.com>
2023-04-09 12:46:23 +0100

    Added getAllBallPassLocationsFromMatch() SQL expr.

--- MCI/mancity/pitch/database/src/insightExpressions.py
+++ MCI/mancity/pitch/database/src/insightExpressions.py
@@ -2,11 +2,11 @@ def getQueryPassesBetweenPlayers(matchID, player1, player2):
     query = """
         SELECT location_x, location_y, minute, second, period
         FROM (
-            SELECT EVENT.id, match_id, EVENT.player_id, location_x, location_y, minute, second, period
+            SELECT EVENT.id, event_type_id, match_id, EVENT.player_id, location_x, location_y, minute, second, period
             FROM EVENT
             JOIN PLAYER ON EVENT.player_id = PLAYER.id
         ) AS T1 JOIN PASS ON PASS.event_id = T1.id
-        WHERE (match_id = """ + str(matchID) + """)
+        WHERE (event_type_id = 30 AND match_id = """ + str(matchID) + """)
         AND ((T1.player_id = """ + str(player1) + """
         AND recipient_id = """+ str(player2) + """)
         OR (T1.player_id = """ + str(player2) + """
@@ -40,6 +40,16 @@ def getAllPassesFromMatch(matchID):
         SELECT PLAYER.name
         FROM EVENT
         JOIN PLAYER ON EVENT.player_id = PLAYER.id
+        WHERE event_type_id = 30 AND match_id = """ + str(matchID) + """
+    """
+    
+    return query
+
+def getAllBallPassLocationsFromMatch(matchID):
+    query = """
+        SELECT location_x, location_y, end_location_x, end_location_y
+        FROM EVENT
+        JOIN PASS ON PASS.event_id = EVENT.id
         WHERE event_type_id = 30 AND match_id = """ + str(matchID) + """
     """
     
