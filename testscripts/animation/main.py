f868893096ad51d424fa9651e0f24ab55e5574ad
harvey <harveybellini@gmail.com>
2023-04-14 21:26:59 +0100

    updated animation

--- testscripts/animation/main.py
+++ testscripts/animation/main.py
@@ -20,19 +20,20 @@ def main(timedata,playercoords):
 
     # Set the animation duration and frame rate, make this depend on length    
     frame_rate = 24  # Frame rate of the animation
-    duration = int(100*frame_rate/(timedata[2]-timedata[0]))  # Animation duration in frames
+    duration = int(20*frame_rate/(timedata[2]-timedata[0]))  # Animation duration in frames
     #total seconds in fr/duration
 
     scalefactor = 47500
 
-    for i in range(22):
+    for i in range(23):
         print(i)
         # Select the object you want to animate
         obj = bpy.context.selected_objects[i]
 
-        coordinates=playercoords[i]
+        coordinates=[]
 
         for data in playercoords[i]:
+            print(data)
             coordinates.append([data[0]*scalefactor,data[1]*scalefactor,data[2]*scalefactor])
 
         # Set keyframes for each coordinate
@@ -41,15 +42,11 @@ def main(timedata,playercoords):
             obj.location = coord
             obj.keyframe_insert(data_path="location", frame=frame_number)
 
-    # dont forget ball
-    ball_obj = bpy.context.selected_objects[22]
 
     # Set the scene's end frame
     bpy.context.scene.frame_end = duration
     gltf_file_path = "testscripts/animation/output.gltf"
     bpy.ops.export_scene.gltf(filepath=gltf_file_path, check_existing=False)
-
-
 
 '''
 take in match time seconds and period for start and end [start seconds, period, endtime seconds, period]
@@ -67,18 +64,59 @@ def player_coords(timedata):
     with open(gamefile, 'r') as json_file:
         json_list = list(json_file)
 
-    for json_str in json_list:
-        result = json.loads(json_str)
-        #print(f"result: {result}")
-        #print(isinstance(result, dict))
+    # vid is at 25 fps
+    time_to_prod = (timedata[2]-timedata[0])*25
+
+    #first half
+    firsthalflist,secondhalflist=[],[]
+    if timedata[1] == 1:
+        for json_str in json_list:
+            result = json.loads(json_str)
+            tmp_list=[]
+            if result['period'] == 1:
+                for element in result['homePlayers']:
+                    tmp_list.append(element['xyz'])
+                for element in result['awayPlayers']:
+                    tmp_list.append(element['xyz'])
+                tmp_list.append(result['ball']['xyz'])
+                firsthalflist.append(tmp_list)
+        ffhalfdata=[[] for x in range(23)]
+        for element in firsthalflist:
+            for i in range(len(element)):
+                ffhalfdata[i].append(element[i])
+        first_half_final=[]
+        for data in ffhalfdata:
+            first_half_final.append(data[:time_to_prod])
+        print("first half data created")
+        return first_half_final
+
+    # second half
+    if timedata[1] == 2:
+        for json_str in json_list:
+            result = json.loads(json_str)
+            tmp_list=[]
+            if result['period'] == 2:
+                for element in result['homePlayers']:
+                    tmp_list.append(element['xyz'])
+                for element in result['awayPlayers']:
+                    tmp_list.append(element['xyz'])
+                tmp_list.append(result['ball']['xyz'])
+                secondhalflist.append(tmp_list)
+        fshalfdata=[[] for x in range(23)]
+        for element in secondhalflist:
+            for i in range(len(element)):
+                fshalfdata[i].append(element[i])  
+        second_half_final=[]
+        for data in fshalfdata:
+            second_half_final.append(data[:time_to_prod])  
+        print("second half data created")
+        return second_half_final
     
-    print(f"result: {len(result['homePlayers'])}")
-    player_coords=[]
-    return
+    
 
 
 if __name__ == '__main__':
-    timedata=[0,1,20]
+    timedata=[0,1,5]
     playercoords= player_coords(timedata)
     main(timedata,playercoords)
     
--- testscripts/animation/makefile.py
+++ testscripts/animation/makefile.py
@@ -4,14 +4,44 @@ gamefile="largefiles/g2312135_SecondSpectrum_tracking-produced.jsonl"
 with open(gamefile, 'r') as json_file:
     json_list = list(json_file)
 
+firsthalflist,secondhalflist=[],[]
+
 for json_str in json_list:
     result = json.loads(json_str)
+    tmp_list=[]
     #print(f"result: {result}")
     #print(isinstance(result, dict))
-print(result['homePlayers'])
-print(result['awayPlayers'])
-print(result['ball'])
+    if result['period'] == 1:
+        for element in result['homePlayers']:
+            tmp_list.append(element['xyz'])
+        for element in result['awayPlayers']:
+            tmp_list.append(element['xyz'])
+        tmp_list.append(result['ball']['xyz'])
+        firsthalflist.append(tmp_list)
+    if result['period'] == 2:
+        for element in result['homePlayers']:
+            tmp_list.append(element['xyz'])
+        for element in result['awayPlayers']:
+            tmp_list.append(element['xyz'])
+        tmp_list.append(result['ball']['xyz'])
+        secondhalflist.append(tmp_list)
 
+ffhalfdata=[[] for x in range(23)]
+fshalfdata=[[] for x in range(23)]
+
+for element in firsthalflist:
+    for i in range(len(element)):
+        ffhalfdata[i].append(element[i])
+
+for element in secondhalflist:
+    for i in range(len(element)):
+        fshalfdata[i].append(element[i])
+        
+
+print(len(ffhalfdata),len(fshalfdata))
+time.sleep(10)
+print(len(ffhalfdata[0]))
+print(len(fshalfdata[0]))
 '''
 
 
