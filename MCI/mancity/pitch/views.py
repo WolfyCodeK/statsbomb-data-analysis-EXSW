6bcab2dfa39f2f83b6a27f6fb3f58ca1994bfb54
harvey <harveybellini@gmail.com>
2023-05-19 00:24:27 +0100

    update

--- MCI/mancity/pitch/views.py
+++ MCI/mancity/pitch/views.py
@@ -287,8 +287,6 @@ def download_time(request, time):
     # Serve the GLB file
     response = FileResponse(open(glb_filename, "rb"), content_type="model/gltf-binary")
     response["Content-Disposition"] = f"attachment; filename={matchPeriod}_{total_seconds}.glb"
-    redirect_to_space(request,glb_filename)
-    print("attempted redirect")
     return response
 
 
@@ -298,13 +296,3 @@ def key_passes(passers,balllocs):
     names = [name[0] for name in passers]
     #turn player passes into tuples (from,to)
     tuples_passes = [(names[i], names[i+1]) for i in range(len(names)-1)]
-
-
-def redirect_to_space(request,filename):
-
-    # Construct the URL for the "space" app
-    space_url = "/space/?text={}".format(filename)
-    print("redirecting......")
-
-    # Redirect the user to the "space" app
-    return redirect(space_url)
